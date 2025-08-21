0f94b940bd086f258b94681e0a3dbee2fc4944e3
Emii-lia <fiaromiangaly@gmail.com>
2025-08-21 21:30:10 +0300

    refactor(visualizer): updating bars directly

--- src/main/kotlin/com/github/emiilia/jetbrainmusicplayer/toolWindow/VisualizerPanel.kt
+++ src/main/kotlin/com/github/emiilia/jetbrainmusicplayer/toolWindow/VisualizerPanel.kt
@@ -1,33 +1,14 @@
 package com.github.emiilia.jetbrainmusicplayer.toolWindow
 
 import com.intellij.ui.JBColor
-import java.awt.Color
 import java.awt.Graphics
 import javax.swing.JPanel
 
 class VisualizerPanel: JPanel() {
     private var bars: List<Int> = emptyList()
-    private var displayedBars: MutableList<Double> = mutableListOf()
-
-    private val riseSpeed = 0.5
-    private val fallSpeed = 0.1
 
     fun updateBars(newBars: List<Int>) {
-        if (displayedBars.isEmpty()) {
-            displayedBars = newBars.map { it.toDouble() }.toMutableList()
-        } else {
-            for (i in displayedBars.indices) {
-                val target = newBars[i].toDouble()
-                val current = displayedBars[i]
-
-                displayedBars[i] = if (current < target) {
-                    current + (target - current) * riseSpeed
-                } else {
-                    current - (current - target) * fallSpeed
-                }
-            }
-        }
-        bars = displayedBars.map { it.toInt() }
+        bars = newBars
         repaint()
     }
 
@@ -40,6 +21,7 @@ class VisualizerPanel: JPanel() {
             val barHeight = (value / 100.0 * height).toInt()
             g.color = JBColor.PINK
             g.fillRect(i * barWidth, height - barHeight, barWidth - 2, barHeight)
+
         }
     }
 }
