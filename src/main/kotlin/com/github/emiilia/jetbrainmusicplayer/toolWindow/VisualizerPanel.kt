3f4f878cd77c1352e0e257a51cc99e1426699b86
Emii-lia <fiaromiangaly@gmail.com>
2025-08-21 21:32:26 +0300

    refactor(visualizer): adding smooth update

--- src/main/kotlin/com/github/emiilia/jetbrainmusicplayer/toolWindow/VisualizerPanel.kt
+++ src/main/kotlin/com/github/emiilia/jetbrainmusicplayer/toolWindow/VisualizerPanel.kt
@@ -1,14 +1,32 @@
 package com.github.emiilia.jetbrainmusicplayer.toolWindow
 
-import com.intellij.ui.JBColor
+import java.awt.Color
 import java.awt.Graphics
 import javax.swing.JPanel
 
 class VisualizerPanel: JPanel() {
     private var bars: List<Int> = emptyList()
+    private var displayedBars: MutableList<Double> = mutableListOf()
+
+    private val riseSpeed = 0.75
+    private val fallSpeed = 0.3
 
     fun updateBars(newBars: List<Int>) {
-        bars = newBars
+        if (displayedBars.isEmpty()) {
+            displayedBars = newBars.map { it.toDouble() }.toMutableList()
+        } else {
+            for (i in displayedBars.indices) {
+                val target = newBars[i].toDouble()
+                val current = displayedBars[i]
+
+                displayedBars[i] = if (current < target) {
+                    current + (target - current) * riseSpeed
+                } else {
+                    current - (current - target) * fallSpeed
+                }
+            }
+        }
+        bars = displayedBars.map { it.toInt() }
         repaint()
     }
 
@@ -19,9 +37,8 @@ class VisualizerPanel: JPanel() {
         val barWidth = width / bars.size
         for ((i, value) in bars.withIndex()) {
             val barHeight = (value / 100.0 * height).toInt()
-            g.color = JBColor.PINK
+            g.color = Color.PINK
             g.fillRect(i * barWidth, height - barHeight, barWidth - 2, barHeight)
-
         }
     }
 }
