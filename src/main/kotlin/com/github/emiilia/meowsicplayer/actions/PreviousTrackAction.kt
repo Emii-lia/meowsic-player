e31515b56014bb9b4b9480bedc1c8452d3cd4379
Emii-lia <fiaromiangaly@gmail.com>
2025-08-26 11:05:45 +0300

    fix(actions): fixing actions implementation override warnings

--- src/main/kotlin/com/github/emiilia/meowsicplayer/actions/PreviousTrackAction.kt
+++ src/main/kotlin/com/github/emiilia/meowsicplayer/actions/PreviousTrackAction.kt
@@ -1,11 +1,15 @@
 package com.github.emiilia.meowsicplayer.actions
 
 import com.github.emiilia.meowsicplayer.services.playerctl.PlayerctlService
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 
 class PreviousTrackAction : AnAction(), DumbAware {
+    
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+    
     override fun actionPerformed(e: AnActionEvent) {
         PlayerctlService.previous()
     }
--- src/main/kotlin/com/github/emiilia/meowsicplayer/actions/ShowMusicPlayerAction.kt
+++ src/main/kotlin/com/github/emiilia/meowsicplayer/actions/ShowMusicPlayerAction.kt
@@ -1,15 +1,19 @@
 package com.github.emiilia.meowsicplayer.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.wm.ToolWindowManager
 
 class ShowMusicPlayerAction : AnAction(), DumbAware {
+    
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+    
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val toolWindowManager = ToolWindowManager.getInstance(project)
-        val toolWindow = toolWindowManager.getToolWindow("MusicPlayer")
+        val toolWindow = toolWindowManager.getToolWindow("Meowsic Player")
         
         toolWindow?.let {
             if (it.isVisible) {
