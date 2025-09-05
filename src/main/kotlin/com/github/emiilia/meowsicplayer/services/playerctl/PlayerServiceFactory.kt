d00b5394009e07f9b91af9161d2f708eae634f51
Emii-lia <fiaromiangaly@gmail.com>
2025-09-05 15:30:47 +0300

    feat(player-service-factory): handling fallback for unlisted os to try to use linux playerctl

--- src/main/kotlin/com/github/emiilia/meowsicplayer/services/playerctl/PlayerServiceFactory.kt
+++ src/main/kotlin/com/github/emiilia/meowsicplayer/services/playerctl/PlayerServiceFactory.kt
@@ -4,14 +4,20 @@ import com.github.emiilia.meowsicplayer.services.playerctl.platform.MacosPlayerS
 import com.github.emiilia.meowsicplayer.services.playerctl.platform.PlayerctlService
 import com.github.emiilia.meowsicplayer.services.playerctl.platform.WindowsPlayerService
 import com.github.emiilia.meowsicplayer.utils.Platform
+import com.intellij.openapi.diagnostic.Logger
 
 object PlayerServiceFactory {
+    private val logger = Logger.getInstance(PlayerServiceFactory::class.java)
+    
     fun createPlayerService(): PlayerctlServiceInterface {
         return when (Platform.os) {
             Platform.OS.WINDOWS -> WindowsPlayerService()
             Platform.OS.MAC -> MacosPlayerService()
             Platform.OS.LINUX -> PlayerctlService()
-            else -> throw UnsupportedOperationException("Unsupported OS for PlayerctlService")
+            else -> {
+                logger.warn("Unsupported OS detected: ${Platform.os}. Falling back to PlayerctlService (Linux approach).")
+                PlayerctlService()
+            }
         }
     }
 }
